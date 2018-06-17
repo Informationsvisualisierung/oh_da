ee9c6fbbc18f6834d4c3e600bdefcb5583ee10be
stmiscue <inyourmaze123@gmail.com>
2018-06-17 14:16:07 +0200

    frontend
    first frontend design

---
+++ vis_one.js
@@ -0,0 +1,8 @@
+/******
+ * VISUALISIERUNG 1
+ * Author: Mina Schütz (730205), Max Röthig, Laura Puchinger
+ * Gruppe: OH_DA Hochschule Darmstadt, 
+ * Sommersemester 2018
+ * Projekt: Visual Forecast Analytics
+ * Betreuer: Prof. Dr. Ing.- Kawa Nazemi
+*****/
