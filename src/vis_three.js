d3c9ed6cd96c3fda6b764c626600511de808e4bb
Mina <inyourmaze123@gmail.com>
2018-07-07 19:39:17 +0200

    update

--- src/vis_three.js
+++ src/vis_three.js
@@ -6,3 +6,16 @@
  * Projekt: Visual Forecast Analytics
  * Betreuer: Prof. Dr. Ing.- Kawa Nazemi
 *****/
+
+		$(document).ready(function(){console.log("hiho");
+		var ourData = ["die daten"];
+		console.log(ourData);
+		d3.json("chocolate.json", function(ourData){
+				console.log(ourData);
+				console.log("hiho no 2");		
+                console.log(treemap);		
+                return ourData;
+			});
+            console.log("Hallo no 3");
+		});
+		console.log("Hallo Ende.");
